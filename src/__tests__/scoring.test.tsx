b70712fc6a07e68120dff90ed29fcce23781f8e0
Shaun Stoll <ushaunstoll@gmail.com>
2025-05-19 22:43:22 +0300

    [TEST] Refactored to less coupled test

--- src/__tests__/scoring.test.tsx
+++ src/__tests__/scoring.test.tsx
@@ -3,11 +3,23 @@ import ScoringService from "@/services/scoring.service";
 import { mockTeams } from "./mocks/teams.mock";
 import { mockTournament } from "./mocks/tournament.mock";
 import { standingsSnapshot } from "./snapshots/standings.snapshot";
+import { Standing } from "@/interfaces/standing.interface";
+
+function getNamesAndScores(standings: Standing[]) {
+  return standings.map((standing) => {
+    return {
+      name: standing.name,
+      score: standing.score,
+    };
+  });
+}
 
 describe("Scoring Test", () => {
   it("should score standings correctly", () => {
+    const expectedResults = getNamesAndScores(standingsSnapshot);
     const scoringService = new ScoringService();
     const standings = scoringService.getStandings(mockTeams, mockTournament);
-    expect(standings).toEqual(standingsSnapshot);
+    const actualResults = getNamesAndScores(standings);
+    expect(actualResults).toEqual(expectedResults);
   });
 });
